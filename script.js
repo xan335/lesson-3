38888fcbd32e12903e16ed58e41a736e92c9a93e
xan335 <hodievhursedhon@gmail.com>
2024-10-11 20:00:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22];
+
+let numbers = arr.filter(item => typeof item === 'number').length;
+    if (numbers > 5) {
+    console.log('гуд');
+    } else {
+        console.log(numbers);
+        
+    }
